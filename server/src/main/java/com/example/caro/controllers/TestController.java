97baba87a6152a88203169a9d38f717ae48be56e
HungND <hungnguyendoan.2002@gmail.com>
2023-10-15 19:06:59 +0700

    reformat folder

--- server/src/main/java/com/example/caro/controllers/TestController.java
+++ server/src/main/java/com/example/caro/controllers/TestController.java
@@ -3,13 +3,13 @@ package com.example.caro.controllers;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class TestController {
-    @MessageMapping("/hello")
-    @SendTo("topic/demo")
+    @GetMapping("test")
     public String sayHello() throws Exception {
-        Thread.sleep(1000);
         return new String("hello bitch");
     }
 }
